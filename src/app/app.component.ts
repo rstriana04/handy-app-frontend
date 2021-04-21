ddba39497cd5ac88b1d2b4496b3677db808e7851
Robert Triana <roberttriana@esvyda.com>
2021-04-21 08:47:50 -0500

    :sparkles: First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'handy-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'handy-app-frontend';
+}
